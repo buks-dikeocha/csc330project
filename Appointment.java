04472c91bad9ba0cdd707c481d0fffe6ea7b4b84
JoseGuzman02 <83370645+JoseGuzman02@users.noreply.github.com>
2022-05-04 17:33:35 -0700

    Update Appointment.java
    added String variable hostID
    Added 2 constructors(didn't want to simplify as scared of breaking something)
    added getDate, getAttendee, getHostID, as well as setHostID to be able to write Appointment into CSV file

--- Appointment.java
+++ Appointment.java
@@ -6,22 +6,40 @@ import java.time.format.DateTimeFormatter;
 public class Appointment {
 	private LocalDate date;
 	private Attendee attendee;
+	private String hostID;
 	
 	public Appointment(Attendee user, String appointmentDate) {
+		init(user, appointmentDate);
+	}
+	public Appointment(Attendee user, LocalDate appointmentDate) {
 		init(user, appointmentDate);
 	}
 	
 	private void init(Attendee user, String appointmentDate) {
 		attendee = user;
 		date = LocalDate.parse(appointmentDate, DateTimeFormatter.ISO_LOCAL_DATE);
+		hostID = null;
+	}
+	private void init(Attendee user, LocalDate appointmentDate) {
+		attendee = user;
+		date = appointmentDate;
+		hostID = null;
 	}
 	
 	public LocalDate getDate() {
 		return date;
 	}
 	
-	public Attendee getAttendee() {
-		return attendee;
+	public String getAttendee() {
+		return attendee.getUserID();
+	}
+	
+	public String getHostID() {
+		return hostID;
+	}
+
+	public void setHostID(String hostID) {
+		this.hostID = hostID;
 	}
 	
 	public String toString() {
