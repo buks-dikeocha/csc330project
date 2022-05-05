ee857ffa85f6a8729092314ab6243f8cda169e08
buks-dikeocha <72320788+buks-dikeocha@users.noreply.github.com>
2022-05-04 20:39:08 -0400

    paths work on all computers, fixed gui

--- User.java
+++ User.java
@@ -6,27 +6,19 @@ import java.util.List;
 
 public abstract class User {
 	protected String userID;
-	protected String firstName;
-	protected String lastName;
 	protected List<Appointment> events;
 	public AccountType accType;
 	public LocalDateTime accCreated;
 	
-	public User(AccountType type, String userName, String firstName, String lastName) {
+	public User(AccountType type, String userName) {
 		this.userID = userName;
-		this.firstName = firstName;
-		this.lastName = lastName;
 		
 		this.events = new ArrayList<Appointment>();
 		this.accCreated = LocalDateTime.now();
 		this.accType = type;
 	}
 	
-	public void scheduler() {}
-	
-	public void descheduler() {}
-	
 	public String toString() {
-		return (accType.name() + " " + firstName + " " + lastName);
+		return (accType.name() + " " + userID);
 	}
 }
